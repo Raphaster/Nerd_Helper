de803aac6d43300fd218cb7a612f0adfb2543b2e
Raphael Reis <raphambmreis@gmail.com>
2021-09-05 20:06:30 -0300

    First commit. Standard react base app

---
+++ app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+  }
+  componentWillUnmount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <label>asas</label>
+      </div>
+    );
+  }
+}
