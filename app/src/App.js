5e740d306dabe1f696998e8eb80928674952136d
Raphael Reis <raphambmreis@gmail.com>
2021-09-08 20:44:45 -0300

    content card base

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import ContentCard from './ContentCard/contentCard.js'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,7 +16,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <label>asas</label>
+        <ContentCard/>
       </div>
     );
   }
---
+++ app/src/ContentCard/contentCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+export default class ContentCard extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render() {
+        var value = 
+        <div>
+            <p>
+                First test.
+            </p>
+        </div>;
+        return value;
+    }
+
+    componentDidMount() {  }
+    componentWillUnmount() {  }
+}
